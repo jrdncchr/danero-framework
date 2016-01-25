e703bfc9839785a772a6f33ccf3768d7b59b789e
Jordan Cachero <jrdncchr@gmail.com>
2016-01-25 13:49:59 +0800

    Update Welcome Page

--- app/Views/welcome.php
+++ app/Views/welcome.php
@@ -1,21 +1,44 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Danero</title>
-    <meta name="description" content="Just smile and there is nothing you can't overcome.">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="<?php echo base_url() . 'resources/css/html5-css-reset.css'; ?>" />
-    <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-</head>
-<body>
-<section>
-    <p>Welcome to my MVC Framework called Danero!</p>
-</section>
-<footer>Copyright <?php echo date('Y'); ?> Danero</footer>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Danero</title>
+        <meta name="description" content="Just smile and there is nothing you can't overcome.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href='http://fonts.googleapis.com/css?family=Raleway:400,500,600,700,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="<?php echo base_url() . 'resources/bower_components/bootstrap/dist/css/bootstrap.min.css'; ?>" />
+        <link rel="stylesheet" href="<?php echo base_url() . 'resources/bower_components/components-font-awesome/css/font-awesome.min.css'; ?>" />
+        <link rel="stylesheet" href="<?php echo base_url() . 'resources/css/html5-css-reset.css'; ?>" />
+        <link rel="stylesheet" href="<?php echo base_url() . 'resources/css/danero.css'; ?>" />
+        <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <style>
+            .welcome p {
+                margin-top: 100px;
+                text-align: center;
+                font-size: 36px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="wrapper">
+            <section class="welcome">
+                <p>DANER<i class="fa fa-smile-o"></i></p>
+            </section>
+            <div class="push"></div>
+        </div>
+        <footer class="footer">&copy; Copyright <?php echo date('Y'); ?> Danero</footer>
+    </body>
+
+    <!-- Scripts -->
+    <script src="<?php echo base_url() . 'resources/bower_components/jquery/dist/jquery.min.js'; ?>"></script>
+    <script src="<?php echo base_url() . 'resources/bower_components/bootstrap/dist/js/bootstrap.min.js'; ?>"></script>
+    <script src="<?php echo base_url() . 'resources/js/danero.js'; ?>"></script>
+
 </html>
---
+++ resources/js/danero.js
@@ -0,0 +1,9 @@
+// STICKY FOOTER
+$(function(){
+    setColumnSize();
+    $(window).resize(function () { setColumnSize(); });
+});
+
+function setColumnSize() {
+    $('.wrapper').css('min-height', $( window ).height() - 20);
+}
