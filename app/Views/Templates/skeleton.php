fbaa5d9125f3404fbd7e6bdf74b45b8f8cafbd3d
Jordan Cachero <jrdncchr@gmail.com>
2016-03-01 16:25:11 +0800

    Updated Nav Style

--- app/Views/Templates/skeleton.php
+++ app/Views/Templates/skeleton.php
@@ -23,24 +23,32 @@
 <body>
 <div class="wrapper">
 
-    <section class="container">
-        <div class="danero-container">
-            <div class="row">
-                <div class="col-md-12">
-                    <?php if(isset($user)) { ?>
-                        <div class="un-logged">
-                            Welcome back, <a href="<?php echo base_url() . 'user/profile'; ?>"><?php echo $user['name']; ?></a> |
-                            <a href="<?php echo base_url() . 'user/logout'; ?>">Logout</a>
-                        </div>
-                    <?php } else { ?>
-                        <div class="un-logged">
-                            <a href="<?php echo base_url() . 'user/login'; ?>">Log in</a> or
-                            <a href="<?php echo base_url() . 'user/sign_up'; ?>">Sign Up</a>
-                        </div>
-                    <?php } ?>
-                    <h2><a href="<?php echo base_url() . 'memoir'; ?>">MEMOIR</a></h2>
+    <section class="danero-nav">
+        <div class="container">
+            <div class="danero-container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php if(isset($user)) { ?>
+                            <div class="un-logged">
+                                Welcome back, <a href="<?php echo base_url() . 'user/profile'; ?>"><?php echo $user['name']; ?></a> |
+                                <a href="<?php echo base_url() . 'user/logout'; ?>">Logout</a>
+                            </div>
+                        <?php } else { ?>
+                            <div class="un-logged">
+                                <a href="<?php echo base_url() . 'user/login'; ?>">Log in</a> or
+                                <a href="<?php echo base_url() . 'user/sign_up'; ?>">Sign Up</a>
+                            </div>
+                        <?php } ?>
+                        <h2 style="font-weight: bold;"><a href="<?php echo base_url() . 'memoir'; ?>">MEMOIR</a></h2>
+                    </div>
                 </div>
             </div>
+        </div>
+    </section>
+
+    <section class="container">
+
+        <div class="danero-container">
 
             <?php echo $content; ?>
         </div>
